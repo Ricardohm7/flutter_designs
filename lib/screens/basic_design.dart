ce831749e2b0167d1228649bccafe2f70ab64b73
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2022-04-14 14:27:26 -0500

    Diseño básico terminado

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -10,6 +10,14 @@ class BasicDesignScreen extends StatelessWidget {
         //landscape
         Image(image: AssetImage('assets/landscape.jpeg')),
         Title(),
+        //Button Section
+        ButtonSection(),
+        //Description
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Text(
+              'Occaecat nostrud culpa commodo deserunt consectetur mollit magna sint amet consequat proident. Ea sunt cillum officia enim labore veniam non. Fugiat et sint aliqua ipsum. Duis nostrud non nulla duis proident anim laboris duis. Tempor commodo non enim laborum ad proident nulla do ex ea laborum nisi.'),
+        ),
       ]),
     );
   }
@@ -45,6 +53,68 @@ class Title extends StatelessWidget {
           Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(
+        horizontal: 30,
+        vertical: 10,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(
+            icon: Icons.call,
+            text: 'CALL',
+          ),
+          CustomButton(
+            icon: Icons.alt_route,
+            text: 'ROUTE',
+          ),
+          CustomButton(
+            icon: Icons.share,
+            text: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: Colors.blue,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            color: Colors.blue,
+          ),
+        ),
+      ],
     );
   }
 }
