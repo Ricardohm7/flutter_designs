84ebb1644baa36e8c1809179ba8f1ef2a5db9536
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2022-04-15 13:27:13 -0500

    Diseño compuesto -fondo cn gradientes completado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:designs/screens/basic_design.dart';
+import 'package:designs/screens/home_screen.dart';
 import 'package:designs/screens/scroll_design.dart';
 import 'package:flutter/material.dart';
 
@@ -10,10 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'scroll_screen',
+      initialRoute: 'home_screen',
       routes: {
         'basic_design': (_) => BasicDesignScreen(),
         'scroll_screen': (_) => ScrollScreen(),
+        'home_screen': (_) => HomeScreen(),
       },
     );
   }
